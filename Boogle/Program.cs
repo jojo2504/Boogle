82d1cabfd33aa26e3d818695aeb5a881d85238b2
jojo2504 <jojo.duchuy@gmail.com>
2024-11-07 10:51:31 +0100

    demonstration

--- Boogle/Program.cs
+++ Boogle/Program.cs
@@ -12,5 +12,7 @@ static class Program
         // see https://aka.ms/applicationconfiguration.
         ApplicationConfiguration.Initialize();
         Application.Run(new Form1());
+
+        int a;
     }    
 }
